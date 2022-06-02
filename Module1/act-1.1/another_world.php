75e6dd77dd72c242e1e017e6a0617a3e74c9cc23
Habib Hajjem <hhajjem@dom.tti>
2022-06-02 15:16:08 +0100

    edit

--- Module1/act-1.1/another_world.php
+++ Module1/act-1.1/another_world.php
@@ -1,6 +1,6 @@
 <?php
             // echo strftime('%A %d %B %Y %I:%M:%S');
             setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
-            echo strtotime("Mardi 12 Novembre 2013 12:02:21");
+            // echo strtotime("Mardi 12 Novembre 2013 12:02:21");
             echo strftime('%A %d %B %Y, %H:%M');
 ?>
